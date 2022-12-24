0e3793e7324b55c5099a30464ca20d404e84d036
Danial Raza <danialrazafb@gmail.com>
2022-12-24 23:29:11 +0100

    feat(components): handle application modal

---
+++ src/components/modals/applicationModal.js
@@ -0,0 +1,99 @@
+const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle: { Danger, Success },
+  EmbedBuilder,
+} = require("discord.js");
+const { formatMilliseconds } = require("format-ms");
+
+module.exports = {
+  id: "applicationModal",
+  async execute(interaction, client) {
+    try {
+      const { fields, guild, member, user } = interaction;
+      const { config } = client;
+      const { applicationChannel, pendingColor, mentionRole } = config;
+
+      const question1 = fields.getTextInputValue("question1");
+
+      await guild.channels.cache
+        .get(applicationChannel)
+        .send({
+          content: `<@&${mentionRole}>`,
+          embeds: [
+            new EmbedBuilder()
+              .setAuthor({
+                name: guild.name,
+                iconURL: guild.iconURL({ size: 512 }),
+              })
+              .setTitle(`**${user.tag}'s Application**`)
+              .setColor(pendingColor)
+              .setThumbnail(user.displayAvatarURL({ size: 512 }))
+              .setFields(
+                {
+                  name: "Question 1",
+                  value: question1,
+                },
+                {
+                  name: "Application Statistics",
+                  value: [
+                    `> 📋 **Application Duration**: \`${formatMilliseconds(
+                      Date.now() - user.startApplication,
+                      { ignore: ["millisecond"] }
+                    )}\``,
+                    `> 🧑 **User**: ${user}`,
+                    `> 💳 **User ID**: \`${user.id}\``,
+                    `> 🤝 **Member Since**: <t:${parseInt(
+                      member.joinedTimestamp / 1000
+                    )}:R>`,
+                    `> 📅 **Account Created**: <t:${parseInt(
+                      user.createdTimestamp / 1000
+                    )}:R>`,
+                  ].join("\n"),
+                }
+              )
+              .setFooter({
+                text: `${guild.name} | Application`,
+                iconURL: guild.iconURL({ size: 512 }),
+              })
+              .setTimestamp(),
+          ],
+          components: [
+            new ActionRowBuilder().setComponents(
+              new ButtonBuilder()
+                .setCustomId("accept")
+                .setLabel("Accept")
+                .setStyle(Success),
+              new ButtonBuilder()
+                .setCustomId("deny")
+                .setLabel("Deny")
+                .setStyle(Danger),
+              new ButtonBuilder()
+                .setCustomId("denyWithReason")
+                .setLabel("Deny With Reason")
+                .setStyle(Danger)
+            ),
+          ],
+        })
+        .then(async (msg) => {
+          msg.startThread({
+            name: `${user.tag}'s Application`,
+            autoArchiveDuration: 1440,
+            reason: `${user.tag}'s Application`,
+          });
+
+          interaction.reply({
+            content:
+              "Thank you for submitting your application! We'll get back to you as soon as possible.",
+            ephemeral: true,
+          });
+        });
+    } catch (error) {
+      console.log(error);
+      interaction.reply({
+        content: "An error occurred, please try again later.",
+        ephemeral: true,
+      });
+    }
+  },
+};
