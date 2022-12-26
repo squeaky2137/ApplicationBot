fca91aef20d0bf2098cd0824e4b8d631acde1c5c
Danial Raza <danialrazafb@gmail.com>
2022-12-26 23:26:55 +0100

    feat(components): handle accept button

---
+++ src/components/buttons/accept.js
@@ -0,0 +1,51 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  id: "accept",
+  async execute(interaction, client) {
+    try {
+      const { config } = client;
+      const { acceptedColor, acceptedRoles, requiredRoles } = config;
+      const { guild, message, user } = interaction;
+
+      const applicant = await guild.members.fetch(
+        message.embeds[0].fields[1].value
+          .split("\n")[2]
+          .split(" ")[4]
+          .replace("`", "")
+          .replace("`", "")
+      );
+
+      await applicant.roles.remove(requiredRoles);
+      await applicant.roles.add(acceptedRoles);
+      message.edit({
+        content: `Application accepted by ${user}`,
+        embeds: [EmbedBuilder.from(message.embeds[0]).setColor(acceptedColor)],
+        components: [],
+      });
+      applicant.send({
+        content: `Congratulations, your application has been accepted!`,
+      });
+      if (message.thread.messages.cache.size <= 1) {
+        message.thread.delete();
+      } else {
+        message.thread.setLocked(true, "Application accepted");
+        message.thread.setArchived(true, "Application accepted");
+      }
+    } catch (error) {
+      if (error.code === 10007) {
+        interaction.reply({
+          content: "The applicant is no longer in the server",
+          ephemeral: true,
+        });
+      } else {
+        console.log(error);
+        interaction.reply({
+          content:
+            "An unknown error occurred, check the console for more information.",
+          ephemeral: true,
+        });
+      }
+    }
+  },
+};
